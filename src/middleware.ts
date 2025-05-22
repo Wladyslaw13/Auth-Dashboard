d3a7c217f5047cfa98eb62e9017bed32e732e8db
VladislavPopov13 <168957070+VladislavPopov13@users.noreply.github.com>
2025-05-22 17:35:03 +0300

    Initial dev work

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,39 @@ import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-	const token = await getToken({
-		req: request,
-		secret: process.env.NEXTAUTH_SECRET,
-	})
+	const env = process.env.VERCEL_ENV
 
-	// Check if the path is the profile page
+	if (env === 'development' || env === 'preview') {
+		const username = process.env.DEV_AUTH_USERNAME || ''
+		const password = process.env.DEV_AUTH_PASSWORD || ''
+		const basicAuth = request.headers.get('authorization')
+		const expected =
+			'Basic ' + Buffer.from(`${username}:${password}`).toString('base64')
+
+		if (basicAuth !== expected) {
+			return new NextResponse('Unauthorized', {
+				status: 401,
+				headers: {
+					'WWW-Authenticate': 'Basic realm="Secure Dev/Preview Area"',
+				},
+			})
+		}
+	}
+
 	if (request.nextUrl.pathname.startsWith('/profile')) {
+		const token = await getToken({
+			req: request,
+			secret: process.env.NEXTAUTH_SECRET,
+		})
+
 		if (!token) {
-			// Redirect to login if not authenticated
 			return NextResponse.redirect(new URL('/login', request.url))
 		}
 	}
 
-	// Allow public pages
 	return NextResponse.next()
 }
 
 export const config = {
-	matcher: ['/profile/:path*'],
+	matcher: ['/profile/:path*', '/((?!_next|favicon.ico|api).*)'],
 }
