54f6e206f4a348c8c78d64111cc29122b491f87b
VladislavPopov13 <168957070+VladislavPopov13@users.noreply.github.com>
2025-05-26 00:37:37 +0300

    deploy fix

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
+import dynamic from 'next/dynamic'
 
-export default function AuthProvider({
+const AuthProvider = ({
 	children,
 	session,
 }: {
 	children: React.ReactNode
 	session: Session | null
-}) {
+}) => {
 	return <SessionProvider session={session}>{children}</SessionProvider>
 }
+
+export default dynamic(() => Promise.resolve(AuthProvider), { ssr: false })
