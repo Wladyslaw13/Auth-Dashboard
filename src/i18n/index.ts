04ecd40627e47332240522ef5772c249d4a37972
VladislavPopov13 <168957070+VladislavPopov13@users.noreply.github.com>
2025-05-27 02:35:09 +0300

    i18n fix

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,20 +1,27 @@
 'use client'
-
 import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
 import en from './en.json'
 import ru from './ru.json'
 
-i18n.use(initReactI18next).init({
-	fallbackLng: 'en',
-	supportedLngs: ['en', 'ru'],
-	resources: {
-		en: { translation: en },
-		ru: { translation: ru },
-	},
-	debug: false,
-	interpolation: { escapeValue: false },
-})
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		fallbackLng: 'en',
+		supportedLngs: ['en', 'ru'],
+		resources: {
+			en: { translation: en },
+			ru: { translation: ru },
+		},
+		detection: {
+			order: ['localStorage', 'navigator'],
+			caches: ['localStorage'],
+		},
+		debug: false,
+		interpolation: { escapeValue: false },
+	})
 
 export default i18n
