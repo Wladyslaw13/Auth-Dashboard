9b52b029c49eef3882f122b08b444e71489f2582
VladislavPopov13 <168957070+VladislavPopov13@users.noreply.github.com>
2025-05-31 02:31:52 +0300

    minor fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,6 +28,8 @@ export async function middleware(request: NextRequest) {
 		})
 
 		console.log(JSON.stringify(token), 'TOKEN HERE', process.env.NODE_ENV, 'NODE_ENV HERE')
+		console.log('COOKIES:', request.cookies.getAll())
+		console.log('HEADERS:', request.headers.get('cookie'))
 
 		if (!token) {
 			return NextResponse.redirect(new URL('/login', request.url))
